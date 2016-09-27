ca7eccc9e52c044a2d8e01f824082c383fc14351
Syrus Akbary <me@syrusakbary.com>
2016-09-26 21:34:01 -0700

    Fixed GA tracking

--- html.js
+++ html.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DocumentTitle from 'react-document-title';
 import classnames from 'classnames';
 import docsTemplate from './pages/_docs_template.html';
+import { config } from 'config';
 
 import { prefixLink } from 'gatsby-helpers'
 
@@ -27,13 +28,13 @@ export default class Html extends React.Component {
           <script src={prefixLink("/bundle.js")}/>
           {isDocs?<script dangerouslySetInnerHTML={{__html: `window.isDocs = true;`}} />:null}
           {isDocs?<div id="docs" dangerouslySetInnerHTML={{__html: docsTemplate.body}} />:null}
-          {this.props.ga?<script dangerouslySetInnerHTML={{__html: `
+          {config.ga?<script dangerouslySetInnerHTML={{__html: `
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', '${this.props.config.ga}', 'auto');
+            ga('create', '${config.ga}', 'auto');
             ga('send', 'pageview');
             `}}
           />:null}
