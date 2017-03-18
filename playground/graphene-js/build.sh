49da35826e949c522eaf9eff8309c025c09d4251
Syrus Akbary <me@syrusakbary.com>
2017-03-18 15:01:32 -0700

    Added typing to the pypyjs bundle

--- playground/graphene-js/build.sh
+++ playground/graphene-js/build.sh
@@ -16,6 +16,7 @@ GRAPHQL_DIR="$(python -c "import os; import graphql; print os.path.dirname(graph
 GRAPHQL_RELAY_DIR="$(python -c "import os; import graphql_relay; print os.path.dirname(graphql_relay.__file__)")"
 PROMISE_DIR="$(python -c "import os; import promise; print os.path.dirname(promise.__file__)")"
 SIX_DIR="$(python -c "import os; import six; print six.__file__.rstrip('c')")"
+TYPING_DIR="$(python -c "import os; import typing; print typing.__file__.rstrip('c')")"
 
 cd pypyjs-release-nojit
 
@@ -28,6 +29,7 @@ python tools/module_bundler.py add --include threading ./lib/modules ../threadin
 python tools/module_bundler.py add --include subprocess ./lib/modules ../subprocess.py
 eval python tools/module_bundler.py add ./lib/modules "$PROMISE_DIR"
 eval python tools/module_bundler.py add ./lib/modules "$SIX_DIR"
+eval python tools/module_bundler.py add ./lib/modules "$TYPING_DIR"
 
 python ./tools/module_bundler.py preload ./lib/modules graphene
 python ./tools/module_bundler.py preload ./lib/modules graphene.relay
@@ -37,6 +39,7 @@ python ./tools/module_bundler.py preload ./lib/modules threading
 python ./tools/module_bundler.py preload ./lib/modules subprocess
 python ./tools/module_bundler.py preload ./lib/modules promise
 python ./tools/module_bundler.py preload ./lib/modules six
+python ./tools/module_bundler.py preload ./lib/modules typing
 
 python ./tools/module_bundler.py remove ./lib/modules unittest
 
