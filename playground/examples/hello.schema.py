e3e9e43361f3789f832d34aed93571013c0df7a5
Miguel <miguel.almonte1@gmail.com>
2017-09-09 18:21:32 -0400

    Update example hello.schema.py to match graphene 2.0.dev
    Example not able to run due to extra parameters in the resolver. I took the example of the README in graphene repository.

--- playground/examples/hello.schema.py
+++ playground/examples/hello.schema.py
@@ -1,9 +1,9 @@
 import graphene
 
 class Query(graphene.ObjectType):
-    hello = graphene.String()
+    hello = graphene.String(description='A typical hello world')
 
-    def resolve_hello(self, args, context, info):
+    def resolve_hello(self, info):
         return 'World'
 
 schema = graphene.Schema(query=Query)
